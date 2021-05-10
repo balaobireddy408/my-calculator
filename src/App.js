d632cf3c668167bcee047032b59554d3f67dab53
balaobireddy408 <balaobireddy408@gmail.com>
2021-05-10 09:37:52 +0530

    test commit

--- src/App.js
+++ src/App.js
@@ -7,7 +7,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
        
-         Bala React Test App,Make a bold move
+         Bala React Test App,Make a bold move, make it fast
        
       </header>
     </div>
